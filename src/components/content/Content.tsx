f20f5a932accfe7186a3022c59eaf918bd05c0a4
hugobatista27 <hugobatista2710@gmail.com>
2023-06-22 11:17:25 -0300

    feat: add o component Skills ao component Content

--- src/components/content/Content.tsx
+++ src/components/content/Content.tsx
@@ -1,7 +1,11 @@
+import styled from 'styled-components';
+
+import Skills from './skills/Skills';
+
 export default function Content() {
     return (
         <>
-            Conteudo
+            <Skills/>
         </>
     )
 }
