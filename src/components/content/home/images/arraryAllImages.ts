cfa76023d56bf7331f0d64c2f9459a8dc75c54f1
hugobatista27 <hugobatista2710@gmail.com>
2023-06-23 14:50:21 -0300

    feat: cria um array com todas as imagens de stack

---
+++ src/components/content/home/images/arraryAllImages.ts
@@ -0,0 +1,17 @@
+import HTML_LOGO from './html-logo.png';
+import CSS_LOGO from './css-logo.png';
+import JAVASCRIPT_LOGO from './javascript-logo.png';
+import REACT_LOGO from './react-logo.png';
+import SASS_LOGO from './sass-logo.png';
+import NODE_LOGO from './node-logo.png';
+
+const STACK_IMGS = [
+    {img: HTML_LOGO, description: "HTML"},
+    {img: CSS_LOGO, description: "CSS"},
+    {img: JAVASCRIPT_LOGO, description: "JavaScript"},
+    {img: REACT_LOGO, description: "ReactJs"},
+    {img: SASS_LOGO, description: "Sass"},
+    {img: NODE_LOGO, description: "NodeJs"}
+]
+
+export { STACK_IMGS }
