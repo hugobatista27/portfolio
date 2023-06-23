3fbe1fae1d21fff4d5f6069a78e2525155b8e922
hugobatista27 <hugobatista2710@gmail.com>
2023-06-23 14:14:52 -0300

    feat: cria e exporta o componente Home

--- src/components/content/Content.tsx
+++ src/components/content/Content.tsx
@@ -1,10 +1,10 @@
-import styled from 'styled-components';
-
 import Skills from './skills/Skills';
+import Home from './projects/Home';
 
 export default function Content() {
     return (
         <>
+            <Home/>
             <Skills/>
         </>
     )
