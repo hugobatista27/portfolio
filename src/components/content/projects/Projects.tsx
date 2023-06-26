5beff1c7f1746cf62170fbc533b7fae562b6fd45
hugobatista27 <hugobatista2710@gmail.com>
2023-06-26 16:34:24 -0300

    feat: cria a estrutura do component Projects

--- src/components/content/projects/Projects.tsx
+++ src/components/content/projects/Projects.tsx
@@ -1,5 +1,8 @@
 import styled from 'styled-components';
 import { ContentWidth } from '../../../styles/global-style';
+import { ProjectsData } from './ProjectsData';
+
+import CardProject from './Card-Project';
 
 const ProjectsStyle = styled.div``;
 
@@ -7,8 +10,10 @@ export default function Projects() {
     return (
         <ProjectsStyle id='projects'>
             <ContentWidth>
-                Projects
-
+                <h1>Projetos</h1>
+                <div>
+                    {ProjectsData.map(project => <CardProject projectInfo={project}/>)}
+                </div>
             </ContentWidth>
         </ProjectsStyle>
     )
