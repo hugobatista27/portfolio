09921040cdfe3959fa3660264cf846ca6c1c1a8e
hugobatista27 <hugobatista2710@gmail.com>
2023-06-28 14:23:34 -0300

    refac: reorganiza a ordem das imagens

--- src/assets/print-projects/ExportImages.ts
+++ src/assets/print-projects/ExportImages.ts
@@ -5,10 +5,10 @@ import kanban3 from './kanban/kanban-mobile-app.jpg';
 import kanban4 from './kanban/kanban-mobile-login.jpg';
 
 // Dicionario
-import dicionario1 from './dicionario/dicionario-desktop-darkmode-searcherror.jpg';
-import dicionario2 from './dicionario/dicionario-desktop-lightmode.jpg';
-import dicionario3 from './dicionario/dicionario-mobile-darkmode.jpg';
-import dicionario4 from './dicionario/dicionario-mobile-lightmode.jpg';
+import dicionario1 from './dicionario/dicionario-desktop-lightmode.jpg';
+import dicionario2 from './dicionario/dicionario-mobile-darkmode.jpg';
+import dicionario3 from './dicionario/dicionario-mobile-lightmode.jpg';
+import dicionario4 from './dicionario/dicionario-desktop-darkmode-searcherror.jpg';
 
 const kanbanImg = [kanban1, kanban2, kanban3, kanban4];
 const dicionarioImg = [dicionario1, dicionario2, dicionario3, dicionario4];
