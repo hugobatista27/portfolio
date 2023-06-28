75d6f617e3c9bc28f5dd4bf5a01dd0710e4da690
hugobatista27 <hugobatista2710@gmail.com>
2023-06-28 11:12:42 -0300

    feat: cria a função que passa as imagens. ExpandedImage

--- src/components/content/projects/ExpandedImage.tsx
+++ src/components/content/projects/ExpandedImage.tsx
@@ -1,24 +1,71 @@
 import styled from "styled-components";
+import { CardType } from "./Card-Project";
+import { useReducer } from "react";
 
 type PropType = {
-    showImages: boolean,
+    projectInfo: CardType,
+    showImages: boolean | undefined,
     setShowImages: React.Dispatch<React.SetStateAction<boolean>>
 }
 
 const ExpandedImageStyle = styled.div`
     width: 100%;
     height: 100%;
-    position: absolute;
+    position: fixed;
     top: 0;
+    left: 0;
     background-color: #000000dc;
+    z-index: 5;
 
     button {
         font-size: 40px;
         color: white;
     }
+
+    img {
+        height: 200px;
+    }
 `;
 
-export default function ExpandedImage({showImages, setShowImages}:PropType) {
+const initialState = {
+    count: 0
+};
+
+const INCREMENT = 'INCREMENT';
+const DECREMENT = 'DECREMENT';
+
+const reducer = (state:{count: number}, action:{type: string, payload: number}) => {
+    let position = state.count
+    switch (action.type) {
+        case INCREMENT:
+            position = state.count + 1;
+            if (position === action.payload) {
+                position = 0
+            }
+            break
+        case DECREMENT:
+            position = state.count - 1;
+            if(position < 0) {
+                position = action.payload - 1
+            }
+            break
+        default:
+            throw new Error(`Unsupported action type: ${action.type}`);
+    }
+    return {count: position}
+};
+
+export default function ExpandedImage({projectInfo, showImages, setShowImages}:PropType) {
+
+    const [state, dispatch] = useReducer(reducer, initialState);
+
+    const handleIncrement = (payload:number) => {
+        dispatch({type: INCREMENT, payload: payload});
+    };
+
+    const handleDecrement = (payload:number) => {
+        dispatch({type: DECREMENT, payload: payload})
+    };
 
     if (showImages) {
         return (
@@ -27,9 +74,9 @@ export default function ExpandedImage({showImages, setShowImages}:PropType) {
                     X
                 </button>
                 <div>
-                    <button>&lt;</button>
-                    <img src="" alt="" />
-                    <button>&gt;</button>
+                    <button onClick={() => handleDecrement(projectInfo.img.length)}>&lt;</button>
+                    <img src={projectInfo.img[state.count]} alt="" />
+                    <button  onClick={() => handleIncrement(projectInfo.img.length)}>&gt;</button>
                 </div>
             </ExpandedImageStyle>
         )
