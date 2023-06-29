13b771f8fb3e075d28e595a795e1879aa9e6441d
hugobatista27 <hugobatista2710@gmail.com>
2023-06-29 11:14:07 -0300

    refac: altera as informações do projeto dicionario

--- src/components/content/projects/ProjectsData.ts
+++ src/components/content/projects/ProjectsData.ts
@@ -34,14 +34,13 @@ const ProjectsData: CardType[] = [
         img: Images.dicionario,
         technologies: [
             "react",
-            "node",
-            "mongo",
-            "sass",
+            "typescript",
+            "styledcomponents",
         ],
-        description: "O projeto Kanban, uma aplicação web fullstack baseada na metodologia Kanban, design proposto pelo site Frontend Mentor. Com funcionalidade intuitiva e design responsivo, o site permite a organização e gerenciamento de tarefas de forma eficiente. Os usuários podem criar diferentes projetos, quadros de tarefas com subtarefas e mover esses quadros entre as colunas.",
+        description: "O projeto é um dicionário da língua inglesa que utiliza uma API pública para fornecer informações sobre palavras em inglês. Ao acessar o site do dicionário, os usuários podem digitar uma palavra em inglês na barra de pesquisa e obterão resultados instantâneos da API. Nesse projeto tive primeiro contato com TypeScript e Styled-Components",
         links: {
-            deploy: "https://kanban-livid-seven.vercel.app/",
-            repository: "https://github.com/hugobatista27/Kanban",
+            deploy: "https://dicionario-six.vercel.app/",
+            repository: "https://github.com/hugobatista27/dicionario",
         }
     }
 ]
