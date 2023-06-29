f25269be1ee49cfdf2761f780b4dc59c5dda13e2
hugobatista27 <hugobatista2710@gmail.com>
2023-06-29 14:58:00 -0300

    feat: cria o componente de estilização. Ajusta o tamanho do footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,7 +1,13 @@
+import styled from 'styled-components';
+
+const FooterStyle = styled.footer`
+    height: 150px;
+`;
+
 export default function Footer() {
     return (
-        <>
+        <FooterStyle>
             footer
-        </>
+        </FooterStyle>
     )
 }
