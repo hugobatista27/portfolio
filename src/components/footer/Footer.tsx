f8fbaf030c08b1341df96ee4de9b592ca2d2d1ca
hugobatista27 <hugobatista2710@gmail.com>
2023-06-30 13:20:37 -0300

    feat: adiciona conteúdo e formata. Footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,13 +1,61 @@
 import styled from 'styled-components';
+import { RxLinkedinLogo, RxGithubLogo } from 'react-icons/rx';
+import { ContentWidth } from '../../styles/global-style';
 
 const FooterStyle = styled.footer`
-    height: 150px;
+    border-top: 1px solid #d4d4d4;
+    padding-top: 20px;
+    margin-bottom: 30px;
+    
+
+    .links {
+        margin-top: 16px;
+        & > div a {
+            display: flex;
+            align-items: center;
+            gap: 12px;
+            margin-top: 8px;
+            width: fit-content;
+        }
+
+        svg {
+            width: 24px;
+            height: 25px;
+        }
+        a, svg {
+            color: black;
+        }
+    }
+
+    @media (max-width: 650px) {
+        display: flex;
+        align-items: center;
+        padding-top: 50px;
+    }
 `;
 
 export default function Footer() {
     return (
         <FooterStyle>
-            footer
+            <ContentWidth>
+                <h3>
+                    Hugo Batista de Siqueira - 2023
+                </h3>
+                <div className='links'>
+                    <div>
+                        <a href="https://www.linkedin.com/in/hugobatista27/" target='_blank'>
+                            <RxLinkedinLogo/>
+                            linkedin.com/in/hugobatista27/
+                        </a>
+                    </div>
+                    <div>
+                        <a href="https://github.com/hugobatista27" target='_blank'>
+                            <RxGithubLogo/>
+                            github.com/hugobatista27
+                        </a>
+                    </div>
+                </div>
+            </ContentWidth>
         </FooterStyle>
     )
 }
